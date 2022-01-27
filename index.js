8c1b6f66c62ea0074f3c59e3d8be2af2abac57ce
monibBormon <monibbormon14@gmail.com>
2022-01-27 23:29:28 +0600

    correction done

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 
 // mongo db connection 
 // database 
-const uri = `mongodb+srv://travellersStory:hhoxHETBzBGqbzPg@cluster0.dejzn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dejzn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
